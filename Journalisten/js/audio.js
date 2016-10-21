bd913158b4098b59f31c04f472e190d9ac3c6245
Taylor Rose <taylorj.rose@hotmail.com>
2016-10-21 15:24:51 +1000

    Updated record.html, record.css and audio.js. Added audio instructions to the webpage (currently disabled). Removed some images that are not required.

--- Journalisten/js/audio.js
+++ Journalisten/js/audio.js
@@ -1,4 +1,11 @@
+document.onkeydown= function(key){ reactKey(key); } 
+function reactKey(evt) {
+   if(evt.keyCode== 40) {
+      alert('worked');
+   }
+}
+
 function play() {
-    /var audio = document.getElementById('playSubmit');
-    audio.play();
+    var audio = document.getElementById('playSubmit');
+   audio.play();
 }
