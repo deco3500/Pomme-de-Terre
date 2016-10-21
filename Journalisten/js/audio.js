4ded694cf895970752b29b889f5822d8feca1506
Taylor Rose <taylorj.rose@hotmail.com>
2016-10-21 16:31:53 +1000

    Completed record.html and audio.js. All recordings can be found in /audio as .mp3 files

--- Journalisten/js/audio.js
+++ Journalisten/js/audio.js
@@ -1,11 +1,44 @@
-document.onkeydown= function(key){ reactKey(key); } 
-function reactKey(evt) {
-   if(evt.keyCode== 40) {
-      alert('worked');
-   }
-}
-
+// Submit Button Audio
 function play() {
     var audio = document.getElementById('playSubmit');
    audio.play();
+}
+
+window.addEventListener("keydown", checkKeyPressed, false);
+
+function checkKeyPressed(e) {
+    if (e.keyCode == "49") {
+	// Play Begin Recording with 1
+		document.getElementById('playRecord').play()
+	}
+	
+	else if (e.keyCode == "50") {
+	// Play Pause Recording with 2
+		document.getElementById('playPause').play()
+	}
+	
+	else if (e.keyCode == "51") {
+	// Play Stop Recording with 3
+		document.getElementById('playStop').play()
+	}
+	
+	else if (e.keyCode == "52") {
+        //Play Play with 4
+        document.getElementById('playPlay').play()
+    }
+	
+	else if (e.keyCode == "53") {
+        //Play Refresh with 5
+        document.getElementById('playRefresh').play()
+    }
+	
+	else if (e.keyCode == "54") {
+        //Play Submit with 6
+        document.getElementById('playSubmit').play()
+    }
+	
+	else if (e.keyCode == "55") {
+	// Play Instructions again with 7
+		document.getElementById('playInstructions').play()
+	}
 }
