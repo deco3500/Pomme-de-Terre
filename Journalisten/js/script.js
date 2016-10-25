33379cd7ec6913331d55ea6556761fea5431bc40
Taylor Rose <taylorj.rose@hotmail.com>
2016-10-26 04:33:16 +1000

    Fixed error with hiding/showing of Instructions section of breaking.html in script.js. Updated comments.jpg for breakingVI.html page.

--- Journalisten/js/script.js
+++ Journalisten/js/script.js
@@ -8,98 +8,94 @@ $(document).ready(function(){
 	$(".hide3").hide();
 	
     $(".bodyButton1").click(function(){
-        $(".record1").show()
-        $(".record2").hide()
-        $(".record3").hide()
+        $(".record1").show();
 		$(".hide1").show();
-		$(".hide2").hide();
-		$(".hide3").hide();
 		$(".bodyButton1").hide();
+		$(".insButton1").show();
 		$(".instructions1").hide();
     });
 	
 	$(".bodyButton2").click(function(){
-		$(".record2").show()
-        $(".record1").hide()
-        $(".record3").hide()
+		$(".record2").show();
 		$(".hide2").show();
-		$(".hide1").hide();
-		$(".hide3").hide();
 		$(".bodyButton2").hide();
+		$(".insButton2").show();
 		$(".instructions2").hide();
     });	
 	
 	$(".bodyButton3").click(function(){
-		$(".record3").show()
-        $(".record1").hide()
-        $(".record2").hide()
+		$(".record3").show();
 		$(".hide3").show();
-		$(".hide1").hide();
-		$(".hide2").hide();
 		$(".bodyButton3").hide();
+		$(".insButton3").show();
 		$(".instructions3").hide();
     });	
 	
     $(".hide1").click(function(){
-        $(".record1").hide()
+        $(".record1").hide();
 		$(".hide1").hide();
-		$(".hide2").hide();
-		$(".hide3").hide();
+		$(".insButton1").hide();
+		$(".hideIns1").hide();
+		$(".instructions1").hide();
 		$(".bodyButton1").show();
     });    
 	
 	$(".hide2").click(function(){
-        $(".record2").hide()
+        $(".record2").hide();
 		$(".hide2").hide();
+		$(".insButton2").hide();
+		$(".hideIns2").hide();
+		$(".instructions2").hide();
 		$(".bodyButton2").show();
     });	
 	
 	$(".hide3").click(function(){
-        $(".record3").hide()
+        $(".record3").hide();
 		$(".hide3").hide();
+		$(".insButton3").hide();
+		$(".hideIns3").hide();
+		$(".instructions23").hide();
 		$(".bodyButton3").show();
     });	
 	
 	$(".insButton1").click(function(){
-        $(".insButton1").hide()
-        $(".instructions1").show()
+        $(".insButton1").hide();
+        $(".instructions1").show();
 		$(".hideIns1").show();
     });
 	
 	$(".hideIns1").click(function(){
-        $(".hideIns1").show()
-        $(".instructions1").hide()
+        $(".hideIns1").show();
+        $(".instructions1").hide();
 		$(".insButton1").show();
     });
 	
 	$(".insButton2").click(function(){
-        $(".insButton2").hide()
-        $(".instructions2").show()
+        $(".insButton2").hide();
+        $(".instructions2").show();
 		$(".hideIns2").show();
     });
 	
 	$(".hideIns2").click(function(){
-        $(".hideIns2").show()
-        $(".instructions2").hide()
+        $(".hideIns2").show();
+        $(".instructions2").hide();
 		$(".insButton2").show();
     });
 	
 	$(".insButton3").click(function(){
-        $(".insButton3").hide()
-        $(".instructions3").show()
+        $(".insButton3").hide();
+        $(".instructions3").show();
 		$(".hideIns3").show();
     });
 	
 	$(".hideIns3").click(function(){
-        $(".hideIns3").show()
-        $(".instructions3").hide()
+        $(".hideIns3").show();
+        $(".instructions3").hide();
 		$(".insButton3").show();
     });
 	
 	$(".submit").click(function(){
-        $(".record1").hide()
-		$(".record2").hide();
-		$(".record3").hide();
+        location.reload();
     });	
 
 
