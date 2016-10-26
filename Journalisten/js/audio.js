9a721b7bf6e533517fd30840a66cdfa0f4e700c7
Taylor Rose <taylorj.rose@hotmail.com>
2016-10-26 21:56:25 +1000

    Updated record.html and audio.js so when the user pushes mutiple buttons, the previous voice is paused.

--- Journalisten/js/audio.js
+++ Journalisten/js/audio.js
@@ -2,38 +2,86 @@ window.addEventListener("keydown", checkKeyPressed, false);
 function checkKeyPressed(e) {
     if (e.keyCode == "49") {
 	// Play Begin Recording with 1
-		document.getElementById('playRecord').play()
+		document.getElementById('playRecord').play();
+		document.getElementById('playPause').pause();
+		document.getElementById('playStop').pause();
+		document.getElementById('playPlay').pause();
+		document.getElementById('playRefresh').pause();
+		document.getElementById('playSubmit').pause();
+		document.getElementById('playInstructions').pause();
 	}
 	
 	else if (e.keyCode == "50") {
 	// Play Pause Recording with 2
-		document.getElementById('playPause').play()
+		document.getElementById('playPause').play();
+		document.getElementById('playRecord').pause();
+		document.getElementById('playStop').pause();
+		document.getElementById('playPlay').pause();
+		document.getElementById('playRefresh').pause();
+		document.getElementById('playSubmit').pause();
+		document.getElementById('playInstructions').pause();
 	}
 	
 	else if (e.keyCode == "51") {
 	// Play Stop Recording with 3
-		document.getElementById('playStop').play()
+		document.getElementById('playStop').play();
+		document.getElementById('playRecord').pause();
+		document.getElementById('playPause').pause();
+		document.getElementById('playPlay').pause();
+		document.getElementById('playRefresh').pause();
+		document.getElementById('playSubmit').pause();
+		document.getElementById('playInstructions').pause();
 	}
 	
 	else if (e.keyCode == "52") {
         //Play Play with 4
-        document.getElementById('playPlay').play()
+        document.getElementById('playPlay').play();
+		document.getElementById('playRecord').pause();
+		document.getElementById('playPause').stop();
+		document.getElementById('playStop').pause();
+		document.getElementById('playRefresh').pause();
+		document.getElementById('playSubmit').pause();
+		document.getElementById('playInstructions').pause();
     }
 	
 	else if (e.keyCode == "53") {
         //Play Refresh with 5
-        document.getElementById('playRefresh').play()
+        document.getElementById('playRefresh').play();
+		document.getElementById('playRecord').pause();
+		document.getElementById('playPause').pause();
+		document.getElementById('playStop').pause();
+		document.getElementById('playPlay').pause();
+		document.getElementById('playSubmit').pause();
+		document.getElementById('playInstructions').pause();
     }
 	
 	else if (e.keyCode == "54") {
         //Play Submit with 6
-        document.getElementById('playSubmit').play()
+        document.getElementById('playSubmit').play();
+		document.getElementById('playRecord').pause();
+		document.getElementById('playPause').pause();
+		document.getElementById('playStop').pause();
+		document.getElementById('playRefresh').pause();
+		document.getElementById('playPlay').pause();
+		document.getElementById('playInstructions').pause();
     }
 	
 	else if (e.keyCode == "55") {
 	// Play Instructions again with 7
-		document.getElementById('playInstructions').play()
+		document.getElementById('playInstructions').play();
+		document.getElementById('playSubmit').pause();
+		document.getElementById('playRecord').pause();
+		document.getElementById('playPause').pause();
+		document.getElementById('playStop').pause();
+		document.getElementById('playRefresh').pause();
+		document.getElementById('playPlay').pause();
 	}
+}
+
+// Submit Button Audio
+function play() {
+    var audio = document.getElementById('playSubmit');
+   audio.play();
 }
 
 // Play Breaking1 audio on breakingVI.html
@@ -73,48 +121,4 @@ function buttonChange1() {
 function buttonChange2() {
     $("#play2").show();
     $("#stop2").hide();
-}
-
-// Submit Button Audio
-function play() {
-    var audio = document.getElementById('playSubmit');
-   audio.play();
-}
-
-window.addEventListener("keydown", checkKeyPressed, false);
-function checkKeyPressed(e) {
-    if (e.keyCode == "49") {
-	// Play Begin Recording with 1
-		document.getElementById('playRecord').play()
-	}
-	
-	else if (e.keyCode == "50") {
-	// Play Pause Recording with 2
-		document.getElementById('playPause').play()
-	}
-	
-	else if (e.keyCode == "51") {
-	// Play Stop Recording with 3
-		document.getElementById('playStop').play()
-	}
-	
-	else if (e.keyCode == "52") {
-        //Play Play with 4
-        document.getElementById('playPlay').play()
-    }
-	
-	else if (e.keyCode == "53") {
-        //Play Refresh with 5
-        document.getElementById('playRefresh').play()
-    }
-	
-	else if (e.keyCode == "54") {
-        //Play Submit with 6
-        document.getElementById('playSubmit').play()
-    }
-	
-	else if (e.keyCode == "55") {
-	// Play Instructions again with 7
-		document.getElementById('playInstructions').play()
-	}
 }
